f51a7be4bd1fff1b8d099c993bf6c4542a333620
MiltonEmm008 <merp2067@gmail.com>
2025-07-04 10:27:16 -0700

    port fix

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ app.get("/api/whoami", (req, res) => {
   });
 });
 
+const PORT = process.env.PORT || 3000;
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
